d529b9a7e7885cbe33e03a7a6d0172e0e0037518
Jesus Gastelum <gastelumtec21@gmail.com>
2023-07-20 13:14:02 -0700

    init taskStore to manage tasks

---
+++ newTodoList/src/store/task/taskStore.ts
@@ -0,0 +1,34 @@
+import { create } from 'zustand';
+import { Todo } from '../../modules/task/pages/TaskPage';
+
+interface State {
+    userToken: string | null;
+    todos: Todo[],
+    loading: boolean;
+    initTodos: () => Promise<void>;
+    addTodo: (description: string) => Promise<void>;
+    removeTodo: (todoId: number) => Promise<void>;
+    toggleTodo: (todoId: number) => Promise<void>;
+    updateTodo: (todoId: number, newDescription: string) => Promise<void>;
+}
+
+export const useTasksStore = create<State>((set, get) => ({
+    userToken: null,
+    todos: [],
+    loading: false,
+    initTodos: async() => {
+
+    },
+    addTodo: async(description: string) => {
+
+    },
+    removeTodo: async(todoId: number) => {
+
+    },
+    toggleTodo: async(todoId: number)  => {
+
+    },
+    updateTodo: async(todoId: number, newDescription: string) => {
+        
+    }
+}));
