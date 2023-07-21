40d276066728a1a9c32843a9f0fc10be5e847453
Jesus Gastelum <gastelumtec21@gmail.com>
2023-07-20 23:31:09 -0700

    remove useless interceptor on authApi

--- newTodoList/src/api/authApi.ts
+++ newTodoList/src/api/authApi.ts
@@ -3,12 +3,3 @@ import axios from "axios";
 export const authApi = axios.create({
     baseURL: 'http://localhost:8080/api/auth'
 })
-
-axios.interceptors.request.use((config) => {
-
-    const token = localStorage.getItem('token');
-
-    config.headers['x-token'] = token;
-
-    return config;
-})
