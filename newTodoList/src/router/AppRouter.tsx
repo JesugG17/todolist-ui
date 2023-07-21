826b478c99dfb49bc389dc9a6e3db43ade084c3d
Jesus Gastelum <gastelumtec21@gmail.com>
2023-07-21 04:25:42 -0700

    add routes protection

--- newTodoList/src/router/AppRouter.tsx
+++ newTodoList/src/router/AppRouter.tsx
@@ -1,12 +1,26 @@
 import { Navigate, Route, Routes } from "react-router";
 import { AuthRoutes } from "../modules/auth/routes/AuthRoutes";
 import { TaskPage } from "../modules/task/pages/TaskPage";
+import { useAuthStore } from "../store/auth/authStore";
 
 export const AppRouter = () => {
+  
+  const status = useAuthStore(state => state.status);
+  
   return (
     <Routes>
-        <Route path="/auth/*" element={ <AuthRoutes /> }/>
-        <Route path="/task" element={ <TaskPage /> } />
+        {
+          status === 'non-authorized' &&
+          (
+            <Route path="/auth/*" element={ <AuthRoutes /> }/>
+          )
+        }
+        {
+          status === 'authorized' &&
+          (
+            <Route path="/task" element={ <TaskPage /> } />
+          )
+        }
         <Route path="/*" element={ <Navigate to='/auth/login' />} />
     </Routes>
   )
