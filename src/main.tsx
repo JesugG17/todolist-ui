e198b07f01e313840babf1a07eed55cb2c5295a5
Jesus Gastelum <gastelumtec21@gmail.com>
2023-07-28 11:29:50 -0700

    fix problem with message when you try to delete completed tasks

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client'
 import { Toaster  } from 'react-hot-toast';
 
 import './styles/index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AppRouter } from './router/AppRouter'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId='669859534983-n7t6t1sdjerndk0tlkrgvqhkbjlqs21c.apps.googleusercontent.com'>
-      <BrowserRouter>
+      <HashRouter>
         <Toaster />
         <AppRouter />
-      </BrowserRouter>
+      </HashRouter>
     </GoogleOAuthProvider>
   </React.StrictMode>,
 )
